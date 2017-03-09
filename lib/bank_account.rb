7ed9bb0e1dab79c8aa24a299d11ec1668754486a
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-03-09 13:53:42 +0000

    Created Clientclass, tests passing

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,46 +1,33 @@
 class Bank
 
-  attr_reader :balance
-  attr_reader :transactions
-  attr_reader :current_transaction
+  # attr_reader :balance
+  # attr_reader :transactions
+  # attr_reader :current_transaction
+
+DATE = Time.now.strftime("%d/%m/%Y")
 
 
-  def initialize
-    @balance = 0
-    @transactions = []
+  def deposit(client, amount)
+     client.plus_balance amount
+     client.transactions.push([DATE, amount,"", client.balance])
   end
 
-  def deposit(amount)
-     (amount >= 0) ? (@balance += amount) : raise('You can not deposit a negative amount')
-     self.data(amount, 'deposit')
+  def withdraw(client, amount)
+    client.minus_balance value
+    client.transactions.push([DATE, amount,"",client.balance])
   end
 
-  def withdraw(amount)
-    (amount >= 0) ? (@balance -= amount) : raise('You cannot withdraw a negative amount')
-    self.data(amount, 'withdraw')
-  end
 
-  def statement
-      top = "date || credit || debit || balance\n"
-      list = ""
-      @transactions.reverse_each do |transaction|
-        if transaction[:type] == 'deposit'
-          list += transaction[:date] + " || " + '%.2f' % transaction[:amount].to_s + " || || " + '%.2f' % transaction[:balance].to_s + "\n"
-        else
-          list += transaction[:date] + " || || " + '%.2f' % transaction[:amount].to_s + " || " + '%.2f' % transaction[:balance].to_s + "\n"
-        end
-      end
-      top + list
-    end
 
-    def data(amount, type)
-      @current_transaction = {}
-      @current_transaction[:type] = type
-      @current_transaction[:date] = Time.now.strftime("%d/%m/%Y")
-      @current_transaction[:amount] = amount
-      @current_transaction[:balance] = @balance
-      @transactions << @current_transaction
-    end
+    # def data(amount, type)
+    #   @current_transaction = {}
+    #   @current_transaction[:date] = Time.now.strftime("%d/%m/%Y")
+    #   @current_transaction[:type] = type
+    #   @current_transaction[:amount] = amount
+    #   @current_transaction[:balance] = @balance
+    #   @transactions << @current_transaction
+    #   @transactions
+    # end
 
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,61 +1,61 @@
 require './lib/bank_account'
 
-describe Bank do
-
-  before(:each) do
-    @bank = Bank.new
-  end
-
-  describe '#balance' do
-    it 'returns current balance' do
-      expect(@bank.balance).to be_an Integer
-    end
-    it 'has an initial bank balance of 0' do
-      expect(@bank.balance).to eq 0
-    end
-  end
-
-  describe '#deposit' do
-    it 'increases the balance by the amount stated (1000)' do
-      expect{@bank.deposit(1000)}.to change{@bank.balance}.by(1000)
-    end
-    it 'does not increase the balance if 0 is deposited' do
-      expect{@bank.deposit(0)}.to change{@bank.balance}.by(0)
-    end
-    it 'does not accept accept negative numbers' do
-      expect{@bank.deposit(-1)}.to raise_error "You can not deposit a negative amount"
-    end
-  end
-
-  describe '#withdraw' do
-
-    before(:each) do
-      @bank.deposit(500)
-    end
-
-    it 'decreases a withdrawn amount from the balance (ex. 500)' do
-      expect{@bank.withdraw(500)}.to change{@bank.balance}.by(-500)
-    end
-
-    it 'does not decrease the balance if 0 is withdrawn' do
-      expect{@bank.withdraw(0)}.to change{@bank.balance}.by(0)
-    end
-
-    it 'does not accept accept negative numbers' do
-      expect{@bank.withdraw(-1)}.to raise_error 'You cannot withdraw a negative amount'
-    end
-  end
-
-  describe '#current_transaction' do
-    before(:each) do
-      @bank.deposit(500)
-    end
-    it 'creates a hash of data for current withdrawal' do
-      expect(@bank.current_transaction).to be_a Hash
-    end
-    it 'stores date, amount and balance into the hash' do
-      time = Time.now.strftime("%d/%m/%Y")
-      expect(@bank.current_transaction).to include(:date => time, :amount => 500, :balance => 500)
-    end
-  end
-end
+# describe Bank do
+#
+#   before(:each) do
+#     @bank = Bank.new
+#   end
+#
+#   describe '#balance' do
+#     it 'returns current balance' do
+#       expect(@bank.balance).to be_an Integer
+#     end
+#     it 'has an initial bank balance of 0' do
+#       expect(@bank.balance).to eq 0
+#     end
+#   end
+#
+#   describe '#deposit' do
+#     it 'increases the balance by the amount stated (1000)' do
+#       expect{@bank.deposit(1000)}.to change{@bank.balance}.by(1000)
+#     end
+#     it 'does not increase the balance if 0 is deposited' do
+#       expect{@bank.deposit(0)}.to change{@bank.balance}.by(0)
+#     end
+#     it 'does not accept accept negative numbers' do
+#       expect{@bank.deposit(-1)}.to raise_error "You can not deposit a negative amount"
+#     end
+#   end
+#
+#   describe '#withdraw' do
+#
+#     before(:each) do
+#       @bank.deposit(500)
+#     end
+#
+#     it 'decreases a withdrawn amount from the balance (ex. 500)' do
+#       expect{@bank.withdraw(500)}.to change{@bank.balance}.by(-500)
+#     end
+#
+#     it 'does not decrease the balance if 0 is withdrawn' do
+#       expect{@bank.withdraw(0)}.to change{@bank.balance}.by(0)
+#     end
+#
+#     it 'does not accept accept negative numbers' do
+#       expect{@bank.withdraw(-1)}.to raise_error 'You cannot withdraw a negative amount'
+#     end
+#   end
+#
+#   describe '#current_transaction' do
+#     before(:each) do
+#       @bank.deposit(500)
+#     end
+#     it 'creates a hash of data for current withdrawal' do
+#       expect(@bank.current_transaction).to be_a Hash
+#     end
+#     it 'stores date, amount and balance into the hash' do
+#       time = Time.now.strftime("%d/%m/%Y")
+#       expect(@bank.current_transaction).to include(:date => time, :amount => 500, :balance => 500)
+#     end
+#   end
+# end
